db1523f16b038472a075a7a9605381d3dce14815
Yinr <hssyinr@yeah.net>
2017-06-08 00:21:30 +0800

    add search box

--- js/bind.js
+++ js/bind.js
@@ -1,4 +1,18 @@
 var vmData = new Vue({
     el: '#data',
-    data: medData
+    data: medData,
+    computed: {
+        filteredData: function() {
+            var datas = this.datas;
+            var filterKey = this.searchTxt && this.searchTxt.toLowerCase();
+            if (filterKey) {
+                datas = datas.filter((row) => {
+                    return Object.keys(row).some((key) => {
+                        return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+                    })
+                });
+            }
+            return datas;
+        }
+    }
 })
